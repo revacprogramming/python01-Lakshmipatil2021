b110ecc8d6a73a0f3890ccf63c878482b1ba295f
Lakshmipatil2021 <93110249+Lakshmipatil2021@users.noreply.github.com>
2022-06-06 04:18:17 +0000

    done

--- ActivitySet01/problem07.py
+++ ActivitySet01/problem07.py
@@ -1,3 +1,5 @@
-# Strings
-
-text = "X-DSPAM-Confidence:    0.8475"
+text = "X-DSPAM-Confidence: 0.8475";
+x=text.find(":")
+fpstring = text[x+1:]
+fvalue=float(fpstring)
+print(fvalue)
--- ActivitySet01/problem08.py
+++ ActivitySet01/problem08.py
@@ -1,3 +1,8 @@
-# Files
-
-filename = "dataset/mbox-short.txt"
+# Use the file name mbox-short.txt as the file name
+fname = input("Enter file name: ")
+fh = open(fname)
+for line in fh:
+    if not line.startswith("X-DSPAM-Confidence:"):
+        continue
+    print(line)
+print("Done")
